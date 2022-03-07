855cf3d411c4a0db2856bc1b0386642532f5d820
= <=>
2022-03-07 02:48:57 +0100

    Change postgres to postgresql

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ gravatar = Gravatar(app,
                     )
 
 # # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL1", "sqlite.///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = MySQLAlchemy(app)
 login_manager = LoginManager()
